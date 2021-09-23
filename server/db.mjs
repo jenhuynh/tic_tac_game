48a2ef83781d12b43e3a433dca939b4fb5ac011e
jenhuynh <jendoubleh@gmail.com>
2021-09-23 11:26:24 -0700

    add user route/path

--- server/db.mjs
+++ server/db.mjs
@@ -8,13 +8,19 @@ export const getTasks = () => db.any("SELECT * FROM tasks");
 export const addTask = (name) =>
   db.one("INSERT INTO tasks(name) VALUES(${name}) RETURNING *", { name });
 
+export const getUsers = () => db.any("SELECT * FROM tictactoe");
+
 export const addUser = ({ player_name }) =>
   db.any(
     "INSERT INTO tictactoe(player_name, x_or_o, number_of_wins) VALUES(${player_name}, ${x_or_o}, ${number_of_wins}) RETURNING *",
     { player_name, x_or_o, number_of_wins },
   );
+
 // select from the table top 5 in descending order
-export const getTopFive = () => db.any("");
+export const getTopFive = () =>
+  db.any(
+    "SELECT number_of_wins FROM tictactoe GROUP BY number_of_wins ORDER BY number_of_wins DESC LIMIT 5",
+  );
 
 function initDb() {
   let connection;
