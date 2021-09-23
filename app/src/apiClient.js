68daa0dec5d7bc19ac64d211506a9c99dd1c431e
jenhuynh <jendoubleh@gmail.com>
2021-09-23 11:18:05 -0700

    Add route to users

--- app/src/apiClient.js
+++ app/src/apiClient.js
@@ -1,19 +1,26 @@
-// export const getTasks = () => _get("/api/tasks");
+export const getTasks = () => _get("/api/tasks");
 
-// export const addTask = (name) => _post("/api/tasks", { name });
+// export const getUsers = () => _get("/api/users");
 
-// const _get = async (url) => (await fetch(url)).json();
+export const getUsers = async () => {
+  const response = await fetch("/api/users");
+  return response.json();
+};
 
-// const _post = async (url, body) => {
-//   const response = await fetch(url, {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(body),
-//   });
-//   let result;
-//   try {
-//     result = await response.json();
-//   } catch {}
+export const addTask = (name) => _post("/api/tasks", { name });
 
-//   return result;
-// };
+const _get = async (url) => (await fetch(url)).json();
+
+const _post = async (url, body) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
+  let result;
+  try {
+    result = await response.json();
+  } catch {}
+
+  return result;
+};
