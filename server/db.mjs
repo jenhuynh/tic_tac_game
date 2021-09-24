066f3ce5e4ee0f3e501bae28c513d1703dc3066b
jenhuynh <jendoubleh@gmail.com>
2021-09-24 11:30:53 -0700

    add updatescore function

--- server/db.mjs
+++ server/db.mjs
@@ -3,18 +3,28 @@ import pgp from "pg-promise";
 
 const db = initDb();
 
-export const getTasks = () => db.any("SELECT * FROM tasks");
+// export const getTasks = () => db.any("SELECT * FROM tasks");
 
-export const addTask = (name) =>
-  db.one("INSERT INTO tasks(name) VALUES(${name}) RETURNING *", { name });
+// export const addTask = (name) =>
+//   db.one("INSERT INTO tasks(name) VALUES(${name}) RETURNING *", { name });
 
-export const getUsers = () => db.any("SELECT * FROM tictactoe");
+//get users in db
+export const getUsers = async () =>
+  await db.any("SELECT * FROM tictactoe ORDER BY number_of_wins DESC LIMIT 5");
+// export const getUsers = () => db.any("SELECT * FROM tictactoe");
 
-export const addUser = ({ player_name, x_or_no }) =>
-  db.one(
-    "INSERT INTO tictactoe(player_name, x_or_o, number_of_wins) VALUES($1, $2) RETURNING *",
+// add users to db
+export const addUser = async ({ player_name, x_or_o }) =>
+  await db.any(
+    "INSERT INTO tictactoe(player_name, x_or_o, number_of_wins) VALUES($1, $2, $3)",
     [player_name, x_or_o, 0],
   );
+
+// export const addUser = ({ player_name, x_or_no }) =>
+//   db.one(
+//     "INSERT INTO tictactoe(player_name, x_or_o, number_of_wins) VALUES($1, $2) RETURNING *",
+//     [player_name, x_or_o, 0],
+//   );
 
 // export const addPlayer = (name) =>
 //   db.one(
@@ -28,17 +38,22 @@ export const addUser = ({ player_name, x_or_no }) =>
 // );
 
 //add user score by one each time
-export const addScore = ({ player_name }) =>
+export const updateScore = ({ player_name }) =>
   db.one(
     "UPDATE tictactoe SET number_of_wins = number_of_wins + 1 WHERE player_name = ${player_name} RETURNING *",
     { player_name },
   );
+// export const addScore = ({ player_name }) =>
+//   db.one(
+//     "UPDATE tictactoe SET number_of_wins = number_of_wins + 1 WHERE player_name = ${player_name} RETURNING *",
+//     { player_name },
+//   );
 
 // select from the table top 5 in descending order
-export const getTopFive = () =>
-  db.any(
-    "SELECT number_of_wins FROM tictactoe GROUP BY number_of_wins ORDER BY number_of_wins DESC LIMIT 5",
-  );
+// export const getTopFive = () =>
+//   db.any(
+//     "SELECT number_of_wins FROM tictactoe GROUP BY number_of_wins ORDER BY number_of_wins DESC LIMIT 5",
+//   );
 
 function initDb() {
   let connection;
