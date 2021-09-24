b044d2bf65cde8c96c124498cb3d4df9afed5c7a
jenhuynh <jendoubleh@gmail.com>
2021-09-24 11:30:12 -0700

    Change post, put, to async await functions

--- app/src/apiClient.js
+++ app/src/apiClient.js
@@ -7,16 +7,34 @@ export const getUsers = async () => {
   return response.json();
 };
 
-export const addUser = ({ player_name, x_or_o }) =>
-  _post("/api/users", { player_name, x_or_o });
+export const addUser = (user) => _post("/api/users", user);
+
+//how we pass info with string interpolation and in backend we receive it in a colon format endput
+export const updateScore = (player_name) => _put(`/api/users/${player_name}`);
 
 // export const addTask = (name) => _post("/api/tasks", { name });
 
-const _get = async (url) => (await fetch(url)).json();
+// const _get = async (url) => (await fetch(url)).json();
 
+//definition of post
 const _post = async (url, body) => {
   const response = await fetch(url, {
     method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
+  let result;
+  try {
+    result = await response.json();
+  } catch {}
+
+  return result;
+};
+
+//definition of put
+const _put = async (url, body) => {
+  const response = await fetch(url, {
+    method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
   });
